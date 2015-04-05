f8839fa3c50f765c89821c4a4c7d41c37c7c7065
antagomir <leo.lahti@iki.fi>
2015-04-05 13:50:31 +0200

    rda moved to microbiome

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,21 +1,7 @@
-# Copyright (C) 2011-2013 Leo Lahti and Jarkko Salojarvi 
-# Contact: <microbiome-admin@googlegroups.com>. All rights reserved.
-
-# This file is a part of the microbiome R package
-# http://microbiome.github.com/
-
-# This program is open source software; you can redistribute it and/or
-# modify it under the terms of the FreeBSD License (keep this notice):
-# http://en.wikipedia.org/wiki/BSD_licenses
-
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
 #' @import parallel
 
 .onAttach <- function(lib, pkg)
 {
-  packageStartupMessage('\nHITChipDB R package (microbiome.github.com)\n(C) 2011-2014 Leo Lahti and Jarkko Salojarvi <microbiome-admin@googlegroups.com>\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the FreeBSD open source license.\n')
+  packageStartupMessage('\nHITChipDB R package (microbiome.github.com)\n(C) 2011-2015 Leo Lahti and Jarkko Salojarvi <microbiome-admin@googlegroups.com>\n')
 
 }
