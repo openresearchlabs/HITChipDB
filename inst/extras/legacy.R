b564ab8cc6dc56eb26e6c3aa8a42fa70fd3a9ad1
antagomir <leo.lahti@iki.fi>
2015-05-19 13:22:06 +0200

    summarize.probesets.species.R to legacy

--- inst/extras/legacy.R
+++ inst/extras/legacy.R
@@ -1,3 +1,38 @@
+probe.summarization <- function (probedata, taxonomy, levels, summarization.methods) {
+
+  # Ensure we have all levels
+  levels <- intersect(levels, colnames(taxonomy))
+
+  # Oligo summarization
+  finaldata <- list()
+  finaldata$oligo <- probedata
+
+  for (level in levels) {
+
+    finaldata[[level]] <- list()
+
+    for (method in summarization.methods) {
+
+        message(paste(level, method))
+	# For species/L1/L2 summarization use the filtered phylogeny: phylogeny.filtered!
+    	summarized.log10 <- summarize.probesets(
+					taxonomy = taxonomy,		
+			    		  oligo.data = log10(probedata), 
+      			       	          method = method, 
+					   level = level)$summarized.matrix
+
+        # Store the data in absolute scale					
+        finaldata[[level]][[method]] <- 10^summarized.log10
+
+    }
+  }
+
+  finaldata
+
+}
+
+
+
 #' Description: summarize.probesets.through.species
 #'
 #' Arguments:
