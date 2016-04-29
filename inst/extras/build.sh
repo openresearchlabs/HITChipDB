14a48d32f088083bbc9e2c51df7b07ffcacda882
antagomir <leo.lahti@iki.fi>
2016-04-29 04:39:33 -0400

    sync

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,7 +1,8 @@
-/usr/local/bin/R CMD BATCH document.R
-/usr/local/bin/R CMD build ../../
-/usr/local/bin/R CMD check HITChipDB_0.5.28.tar.gz
-/usr/local/bin/R CMD INSTALL HITChipDB_0.5.28.tar.gz
+
+~/bin/R-3.2.3/bin/R CMD BATCH document.R
+~/bin/R-3.2.3/bin/R CMD build ../../
+~/bin/R-3.2.3/bin/R CMD check HITChipDB_0.5.29.tar.gz
+~/bin/R-3.2.3/bin/R CMD INSTALL HITChipDB_0.5.29.tar.gz
 
 
 
